e689b966e20c9d26135bc9295722a88bc95f91f1
HeJingjing <campanulamediuml@gmail.com>
2018-07-03 11:12:32 +0800

    jiekou

--- common_methods/db.py
+++ common_methods/db.py
@@ -1,8 +1,8 @@
-host='117.25.155.149'
-port=3306
-user='gelinroot'
-passwd='glt#789A'
-db='dolphERP'
-charset='utf8'
-reject_word_list = ['select','delete','drop table','truncate table']
+host=
+port=
+user=
+passwd=
+db=
+charset=
+reject_word_list =[]
 
